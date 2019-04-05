dfa6bf89e25093241160426bb43d94f621757260
johnpolacek <johnpolacek@gmail.com>
2019-04-05 09:00:56 -0500

    fix formatting of markdown file generated by event submission

--- functions/add-event/add-event.js
+++ functions/add-event/add-event.js
@@ -42,25 +42,24 @@ exports.handler = (event, context, callback) => {
   const filename = dateStr+'-'+body.eventName.toLowerCase().split(' ').join('-')+'.md'
   const filepath = 'content/eventslist/'+filename
 
-  const newContent = `
-    ---
-    title: "${body.eventName}"
-    date: "${date.toISOString()}"
-    startDate: "${body.startDate}"
-    startTime: "${body.startTime}"
-    endDate: "${body.endDate}"
-    endTime: "${body.endTime}"
-    locationName: "${body.locationName}"
-    locationStreet: "${body.locationStreet}"
-    locationCity: "${body.locationCity}"
-    cost: "${body.cost}"
-    eventUrl: "${body.linkURL}"
-    
-    ---
+  const newContent = `---
+title: "${body.eventName}"
+date: "${date.toISOString()}"
+startDate: "${body.startDate}"
+startTime: "${body.startTime}"
+endDate: "${body.endDate}"
+endTime: "${body.endTime}"
+locationName: "${body.locationName}"
+locationStreet: "${body.locationStreet}"
+locationCity: "${body.locationCity}"
+cost: "${body.cost}"
+eventUrl: "${body.linkURL}"
 
-    ${body.description}
+---
 
-  `
+${body.description}
+
+`
 
   octokit.createPullRequest({
     owner,

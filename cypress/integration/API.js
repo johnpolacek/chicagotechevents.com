355f2a90330495329eead2c387aa48db456107ab
johnpolacek <johnpolacek@gmail.com>
2019-04-22 07:03:46 -0500

    set timezone for api test, disable video generation in tests

--- cypress/integration/API.js
+++ cypress/integration/API.js
@@ -1,9 +1,10 @@
 import getEventMarkdown from '../../src/functions/add-event/getEventMarkdown'
 import { getValidEventData } from '../support/helpers'
+import { DateTime } from 'luxon'
 
 describe('API', function() {
   it('can generate markdown from valid event data', function() {
-  	const submitDate = new Date().toISOString()
+  	const submitDate = DateTime.local().setZone('America/Chicago').toString()
   	const validEventData = {...getValidEventData(), ...{date: submitDate} }
   	const validEventDataMarkdownOutput = '---\ntitle: "Test Event"\ndate: "'+submitDate+'"\nstartDate: "Wed May 01 2019 00:00:00 GMT-0500 (Central Daylight Time)"\nstartTime: "5:00pm"\nendDate: "Wed May 01 2019 00:00:00 GMT-0500 (Central Daylight Time)"\nendTime: "7:00pm"\nlocationName: "1871 Chicago"\nlocationStreet: "222 W Merchandise Mart Plaza #1212"\nlocationCity: "Chicago"\ncost: "FREE"\neventUrl: "https://eventbrite.com/test-event"\n\n---\n\nThis is not a real event. It is just for testing\n\n'
     cy.expect(getEventMarkdown(validEventData)).to.equal(validEventDataMarkdownOutput)

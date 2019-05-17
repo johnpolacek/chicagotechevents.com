d26bb70152bdb7ba43ab5d2c2b39d2c80d7d0ced
johnpolacek <johnpolacek@gmail.com>
2019-05-17 06:24:08 -0500

    debug get-meetups

--- src-functions/get-meetups.js
+++ src-functions/get-meetups.js
@@ -4,19 +4,19 @@ const meetup = require('meetup-api')({
 
 exports.handler = (event, context, callback) => {
 
-  const body = JSON.parse(event.body)
-  if (!body) {
-    return callback(null, {
-      statusCode: 422,
-      body: JSON.stringify({
-        data: 'Missing request body'
-      })
-    })
-  }
+  // const body = JSON.parse(event.body)
+  // if (!body) {
+  //   return callback(null, {
+  //     statusCode: 422,
+  //     body: JSON.stringify({
+  //       data: 'Missing request body'
+  //     })
+  //   })
+  // }
 
   return callback(null, {
           statusCode: 200,
-          body: JSON.stringify({ message: `testing`, key: process.env.MEETUP_KEY, body })
+          body: JSON.stringify({ message: `testing` })
         })
 
   // https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public&lon=-87.6298&page=20&text=tech&radius=5&lat=41.8781&offset=0

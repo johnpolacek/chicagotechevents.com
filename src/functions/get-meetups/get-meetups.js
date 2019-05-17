5ced9f880606e2ffac622bb6e948f20695cee5de
johnpolacek <johnpolacek@gmail.com>
2019-05-17 16:47:58 -0500

    fix pagination of meetup results

--- src/functions/get-meetups/get-meetups.js
+++ src/functions/get-meetups/get-meetups.js
@@ -21,7 +21,7 @@ exports.handler = (event, context, callback) => {
     lat: 41.8781,
     lon: -87.6298,
     text: 'tech',
-    offset: '20'
+    offset: body.page,
     radius: 5
   }, function(err, res) {
       if (err) {

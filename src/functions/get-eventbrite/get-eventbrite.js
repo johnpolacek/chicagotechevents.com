d2379813ad2fae3c62efdaf3ec76a2431b763657
johnpolacek <johnpolacek@gmail.com>
2019-05-27 12:31:05 -0500

    debug eventbrite functions

--- src/functions/get-eventbrite/get-eventbrite.js
+++ src/functions/get-eventbrite/get-eventbrite.js
@@ -1,14 +1,19 @@
 const eventbrite = require('eventbrite')
 // const sdk = eventbrite({token: process.env.EVENTBRITE_TOKEN})
 
-// sdk.request('/events/search?q=tech').then(res => {
-//   return callback(null, {
-//     statusCode: 200,
-//     body: JSON.stringify({ message: `success`, response: res })
-//   })
-// })
 
-return callback(null, {
-  statusCode: 200,
-  body: JSON.stringify({ message: `success`, eventbriteTypeOf: (typeof eventbrite), eventbrite: eventbrite })
-})
+exports.handler = (event, context, callback) => {
+	
+	// sdk.request('/events/search?q=tech').then(res => {
+	//   return callback(null, {
+	//     statusCode: 200,
+	//     body: JSON.stringify({ message: `success`, response: res })
+	//   })
+	// })
+	
+	return callback(null, {
+	  statusCode: 200,
+	  body: JSON.stringify({ message: `success`, eventbriteTypeOf: (typeof eventbrite), eventbrite: eventbrite })
+	})
+}
+

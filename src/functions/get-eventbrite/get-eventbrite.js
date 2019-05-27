19b639d6e4bf211e6e7b00af9c3d6220c3b16574
johnpolacek <johnpolacek@gmail.com>
2019-05-27 14:13:28 -0500

    execute test search in eventbrite function

--- src/functions/get-eventbrite/get-eventbrite.js
+++ src/functions/get-eventbrite/get-eventbrite.js
@@ -1,19 +1,11 @@
 const eventbrite = require('eventbrite').default
-// const sdk = eventbrite({token: process.env.EVENTBRITE_TOKEN})
-
+const sdk = eventbrite({token: process.env.EVENTBRITE_TOKEN})
 
 exports.handler = (event, context, callback) => {
-	
-	// sdk.request('/events/search?q=tech').then(res => {
-	//   return callback(null, {
-	//     statusCode: 200,
-	//     body: JSON.stringify({ message: `success`, response: res })
-	//   })
-	// })
-	
-	return callback(null, {
-	  statusCode: 200,
-	  body: JSON.stringify({ message: `success`, eventbriteTypeOf: (typeof eventbrite), eventbrite: eventbrite })
+	sdk.request('/events/search?q=tech').then(res => {
+	  return callback(null, {
+	    statusCode: 200,
+	    body: JSON.stringify({ message: `success`, response: res })
+	  })
 	})
 }
-

346c93bd4c2cd1de8e2d87e12e16760d2785c97d
johnpolacek <johnpolacek@gmail.com>
2019-05-27 14:49:38 -0500

    require search param in eventbrite search requests

--- src/functions/get-eventbrite/get-eventbrite.js
+++ src/functions/get-eventbrite/get-eventbrite.js
@@ -2,7 +2,26 @@ const eventbrite = require('eventbrite').default
 const sdk = eventbrite({token: process.env.EVENTBRITE_TOKEN})
 
 exports.handler = (event, context, callback) => {
-	sdk.request('/events/search?q=tech').then(res => {
+	const body = JSON.parse(event.body)
+  if (!body) {
+    return callback(null, {
+      statusCode: 422,
+      body: JSON.stringify({
+        data: 'Missing request body'
+      })
+    })
+  }
+
+  if (!body.search) {
+    return callback(null, {
+      statusCode: 422,
+      body: JSON.stringify({
+        data: 'Missing search parameter'
+      })
+    })
+  }
+
+	sdk.request('/events/search?q='+body.search).then(res => {
 	  return callback(null, {
 	    statusCode: 200,
 	    body: JSON.stringify({ message: `success`, response: res })

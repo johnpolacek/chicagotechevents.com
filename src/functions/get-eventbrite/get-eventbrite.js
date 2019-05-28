d3544bfdda8f8f3eab263fefc5f9ffe84a30eeeb
johnpolacek <johnpolacek@gmail.com>
2019-05-27 19:49:47 -0500

    debug eventbrite api request

--- src/functions/get-eventbrite/get-eventbrite.js
+++ src/functions/get-eventbrite/get-eventbrite.js
@@ -21,19 +21,24 @@ exports.handler = (event, context, callback) => {
     })
   }
 
-  try {
-  	sdk.request('/events/search?q='+body.search)
-			.then(response => response.json())
-			.then(data => {
-			  return callback(null, {
-			    statusCode: 200,
-			    body: JSON.stringify({ message: `success`, data: data })
-			  })
-			})
-  } catch(error) {
-  	return callback(null, {
-      statusCode: 422,
-      body: JSON.stringify({error: error})
-    })
-  }
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({ message: `success`, typeof: typeof(sdk), sdk: sdk })
+  })
+
+  // try {
+  // 	sdk.request('/events/search?q='+body.search)
+		// 	.then(response => response.json())
+		// 	.then(data => {
+		// 	  return callback(null, {
+		// 	    statusCode: 200,
+		// 	    body: JSON.stringify({ message: `success`, data: data })
+		// 	  })
+		// 	})
+  // } catch(error) {
+  // 	return callback(null, {
+  //     statusCode: 422,
+  //     body: JSON.stringify({error: error})
+  //   })
+  // }
 }

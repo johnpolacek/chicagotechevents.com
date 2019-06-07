224b8ee70e7e2955e694c0f4e80d7b327feb0a3d
johnpolacek <johnpolacek@gmail.com>
2019-06-07 15:26:39 -0500

    update get eventbrite events api response

--- src/functions/get-eventbrite/get-eventbrite.js
+++ src/functions/get-eventbrite/get-eventbrite.js
@@ -23,11 +23,14 @@ exports.handler = async (event, context, callback) => {
       })
     })
   }
-	
-  const response = await fetch('https://www.eventbriteapi.com/v3/events/search/?q='+body.search+'&location.viewport.northeast.latitude=42.054574&location.viewport.northeast.longitude=-87.537518&location.viewport.southwest.latitude=41.751520&location.viewport.southwest.longitude=-87.712612&date_modified.range_start='+dateRange+'T06%3A50%3A27Z&token='+process.env.EVENTBRITE_TOKEN+'&expand=ticket_classes,venue&page=1')
-  const data = await response.json()
-  return  {
-  	statusCode: 200,
-    body: JSON.stringify({ message: `success`, response: data })
-  }
+
+  fetch('https://www.eventbriteapi.com/v3/events/search/?q='+body.search+'&location.viewport.northeast.latitude=42.054574&location.viewport.northeast.longitude=-87.537518&location.viewport.southwest.latitude=41.751520&location.viewport.southwest.longitude=-87.712612&date_modified.range_start='+dateRange+'T06%3A50%3A27Z&token='+process.env.EVENTBRITE_TOKEN+'&expand=ticket_classes,venue&page=1', { method: 'POST' })
+    .then(res => res.json()) // expecting a json response
+    .then(data => {
+      return  {
+        statusCode: 200,
+        body: JSON.stringify({ message: `success`, response: data })
+      }
+    })
+  
 }

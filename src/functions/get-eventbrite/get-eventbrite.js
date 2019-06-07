9d4035274419e2a512d832e175a6ef1f40d9168c
johnpolacek <johnpolacek@gmail.com>
2019-06-07 06:07:13 -0500

    fix syntax error in get-eventbrite api

--- src/functions/get-eventbrite/get-eventbrite.js
+++ src/functions/get-eventbrite/get-eventbrite.js
@@ -28,7 +28,6 @@ exports.handler = async (event, context, callback) => {
   const data = await response.json()
   return  {
   	statusCode: 200,
-  	body: JSON.stringify(data)
     body: JSON.stringify({ message: `success`, response: data })
   }
 }

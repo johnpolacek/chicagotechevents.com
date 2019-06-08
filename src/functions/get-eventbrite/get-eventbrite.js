446d7ad1470ff38a150e6ddb59275903c6ddb301
johnpolacek <johnpolacek@gmail.com>
2019-06-08 14:00:39 -0500

    update get eventbrite api

--- src/functions/get-eventbrite/get-eventbrite.js
+++ src/functions/get-eventbrite/get-eventbrite.js
@@ -26,28 +26,20 @@ exports.handler = async (event, context, callback) => {
 
   const fetchUrl = 'https://www.eventbriteapi.com/v3/events/search/?q='+body.search+'&location.viewport.northeast.latitude=42.054574&location.viewport.northeast.longitude=-87.537518&location.viewport.southwest.latitude=41.751520&location.viewport.southwest.longitude=-87.712612&date_modified.range_start='+dateRange+'T06%3A50%3A27Z&token='+process.env.EVENTBRITE_TOKEN+'&expand=ticket_classes,venue&page=1'
 
-  return fetch(fetchUrl)
-    .then(res => {
-      console.log('success', res)
-      res.json().then(data => {
-        console.log('data', data);
-          return  {
-          statusCode: 200,
-          body: JSON.stringify({ message: `success`, response: data, url: fetchUrl })
-        }
-      })
-      .catch((error) => {
-        console.log('error', error)
-        return callback(null, {
-          statusCode: 400,
-          body: JSON.stringify(error)
-        })
-      })
-    }).catch((error) => {
-      console.log('error', error)
-      return callback(null, {
-        statusCode: 400,
-        body: JSON.stringify(error)
-      })
+  return fetch(fetchUrl).then(res => {
+    return res.json()
+  }).then(data => {
+    console.log('data', data);
+    return  {
+      statusCode: 200,
+      body: JSON.stringify({ message: `success`, response: data, url: fetchUrl })
+    }
+  }).catch((error) => {
+    console.log('error', error)
+    return callback(null, {
+      statusCode: 400,
+      body: JSON.stringify(error)
     })
+  })
+  
 }

669e78bf744fa3daf2a01a425a89f15cde37d88f
johnpolacek <johnpolacek@gmail.com>
2019-06-24 20:00:14 -0500

    force email header text color

--- src/components/email/Header.js
+++ src/components/email/Header.js
@@ -5,7 +5,7 @@ export default props => (
   <>
     <tr style={{ background: theme.colors.blue7, textAlign: 'center' }}>
       <td style={{ padding: '32px 0 8px' }}>
-        <h1 style={{margin: '8px 0 0', fontWeight:'normal', fontSize: '42px', color: '#fff', textAlign: 'center' }}>{props.title.toUpperCase()}</h1>
+        <h1 style={{margin: '8px 0 0', fontWeight:'normal', fontSize: '42px', color: '#fff !important', textAlign: 'center' }}>{props.title.toUpperCase()}</h1>
       </td>
     </tr>
     <tr>

036fe38d3278540361f95dbcc35a7142feee685f
johnpolacek <johnpolacek@gmail.com>
2019-07-04 16:15:51 -0500

    update add-sponsor function with S3 image upload

--- src/functions/add-sponsor/add-sponsor.js
+++ src/functions/add-sponsor/add-sponsor.js
@@ -9,11 +9,13 @@ exports.handler = async (event, context, callback) => {
   try {
   
     const submitData = JSON.parse(event.body)
-    // const srcData = Buffer.from(submitData.file.replace(/^data:image\/\w+;base64,/, ""), 'base64')
+    const srcData = Buffer.from(submitData.file.replace(/^data:image\/\w+;base64,/, ""), 'base64')
+
     const params = {
       Bucket: 'docqet-images',
-      Key: 'sponsors/test',
-      Body: 'test'
+      Key: 'sponsors/test.jpg',
+      Body: srcData,
+      ContentType: 'image/jpeg'
     }
 
     s3.putObject(params, function(err, data) {

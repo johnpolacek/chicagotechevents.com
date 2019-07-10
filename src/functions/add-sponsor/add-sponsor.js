c5d06b4e6a4bdcc55fb749e4128f1e75f6baa0be
johnpolacek <johnpolacek@gmail.com>
2019-07-10 06:33:35 -0500

    wip test add-sponsor endpoint

--- src/functions/add-sponsor/add-sponsor.js
+++ src/functions/add-sponsor/add-sponsor.js
@@ -6,37 +6,42 @@ const s3 = new AWS.S3({
 
 exports.handler = async (event, context, callback) => {
 
-  try {
-  
-    const submitData = JSON.parse(event.body)
-    const srcData = Buffer.from(submitData.file.replace(/^data:image\/\w+;base64,/, ""), 'base64')
-
-    const params = {
-      Bucket: 'docqet-images',
-      Key: 'sponsors/test.jpg',
-      Body: srcData,
-      ContentType: 'image/jpeg'
-    }
-
-    s3.putObject(params, function(err, data) {
-      if (err) {
-        return  {
-          statusCode: 400,
-          body: JSON.stringify({ message: `Could not upload image`, error: err })
-        }
-      } else {
-        return  {
-          statusCode: 200,
-          body: JSON.stringify({ message: `success` })
-        }
-      }
-    })
-  } catch (err) {
-    return  {
-      statusCode: 400,
-      body: JSON.stringify({ message: `Could not upload image`, error: err })
-    }
+  return  {
+    statusCode: 200,
+    body: JSON.stringify({ message: `success` })
   }
+
+  // try {
+  
+  //   const submitData = JSON.parse(event.body)
+  //   const srcData = Buffer.from(submitData.file.replace(/^data:image\/\w+;base64,/, ""), 'base64')
+
+  //   const params = {
+  //     Bucket: 'docqet-images',
+  //     Key: 'sponsors/test.jpg',
+  //     Body: srcData,
+  //     ContentType: 'image/jpeg'
+  //   }
+
+  //   s3.putObject(params, function(err, data) {
+  //     if (err) {
+  //       return  {
+  //         statusCode: 400,
+  //         body: JSON.stringify({ message: `Could not upload image`, error: err })
+  //       }
+  //     } else {
+  //       return  {
+  //         statusCode: 200,
+  //         body: JSON.stringify({ message: `success` })
+  //       }
+  //     }
+  //   })
+  // } catch (err) {
+  //   return  {
+  //     statusCode: 400,
+  //     body: JSON.stringify({ message: `Could not upload image`, error: err })
+  //   }
+  // }
 }
 
  

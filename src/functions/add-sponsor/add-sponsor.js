805d368a843792a8a523b736451f8cebeaf25c5d
johnpolacek <johnpolacek@gmail.com>
2019-07-12 06:20:18 -0500

    wip add sponsor api

--- src/functions/add-sponsor/add-sponsor.js
+++ src/functions/add-sponsor/add-sponsor.js
@@ -9,28 +9,40 @@ exports.handler = async (event, context, callback) => {
   try {
   
     const submitData = JSON.parse(event.body)
-    const srcData = Buffer.from(submitData.file.replace(/^data:image\/\w+;base64,/, ""), 'base64')
+    if (typeof(submitData.file !== 'undefined')) {
+      const srcData = Buffer.from(submitData.file.replace(/^data:image\/\w+;base64,/, ""), 'base64')
 
-    const params = {
-      Bucket: 'docqet-images',
-      Key: 'sponsors/test.jpg',
-      Body: srcData,
-      ContentType: 'image/jpeg'
+      const params = {
+        Bucket: 'docqet-images',
+        Key: 'sponsors/test.jpg',
+        Body: srcData,
+        ContentType: 'image/jpeg'
+      }
+      return  {
+        statusCode: 200,
+        body: JSON.stringify({ params: params })
+      }
+    } else {
+      return  {
+        statusCode: 200,
+        body: JSON.stringify({ message: `success` })
+      }
     }
 
-    s3.putObject(params, function(err, data) {
-      if (err) {
-        return  {
-          statusCode: 500,
-          body: JSON.stringify({ message: `Could not upload image`, error: err })
-        }
-      } else {
-        return  {
-          statusCode: 200,
-          body: JSON.stringify({ message: `success` })
-        }
-      }
-    })
+
+    // s3.putObject(params, function(err, data) {
+    //   if (err) {
+    //     return  {
+    //       statusCode: 500,
+    //       body: JSON.stringify({ srcData: srcData, message: `putObject Error: Could not upload image`, error: err })
+    //     }
+    //   } else {
+    //     return  {
+    //       statusCode: 200,
+    //       body: JSON.stringify({ message: `success` })
+    //     }
+    //   }
+    // })
 
   } catch (err) {
     return  {

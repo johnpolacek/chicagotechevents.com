75c525dd6ccd763760fccb545241944247156e22
johnpolacek <johnpolacek@gmail.com>
2019-07-29 11:00:35 -0500

    fix email header preview render

--- src/components/email/Header.js
+++ src/components/email/Header.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import theme from '../../theme'
+import { Div } from 'styled-system-html'
 
 export default props => (
   <>
-    <tr style={{ background: theme.colors.blue7, textAlign: 'center' }}>
-      <td style={{ padding: '32px 0 8px', margin: '8px 0 0', fontWeight:'normal', fontSize: '42px', color: '#fff', textAlign: 'center' }}>{props.title.toUpperCase()}</td>
-    </tr>
-    <tr>
-      <td>
-        <img alt="Chicago Stars and Skyline" style={{width:'100%',height:'auto'}} src="https://chicagotechevents.netlify.com/img/email-header.gif" />
-      </td>
-    </tr>
+    <Div style={{ background: theme.colors.blue7, textAlign: 'center' }}>
+      <Div style={{ padding: '32px 0 8px', margin: '8px 0 0', fontWeight:'normal', fontSize: '42px', color: '#fff', textAlign: 'center' }}>{props.title.toUpperCase()}</Div>
+    </Div>
+    <Div>
+      <Div>
+        <img alt="Chicago Stars and Skyline" style={{width:'100%',height:'auto'}} src="/img/email-header.gif" />
+      </Div>
+    </Div>
   </>
 )

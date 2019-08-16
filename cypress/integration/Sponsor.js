c322c7aca92505f301c34005c4270bb0d49cf7de
johnpolacek <johnpolacek@gmail.com>
2019-08-16 15:37:58 -0500

    wip add sponsor tests

--- cypress/integration/Sponsor.js
+++ cypress/integration/Sponsor.js
@@ -1,23 +1,29 @@
-import {
-  deferred,
-  getMeetupResults,
-  getEventbriteResults,
-} from '../support/helpers'
+import getSponsorMarkdown from '../../src/functions/add-sponsor/getSponsorMarkdown'
+import { deferred } from '../support/helpers'
 
 describe('Sponsor', function() {
 
   it('ad when inactive shows promo', function() {
-    
+    // cy.exec move any existing sponsor md file into temp then back
+    // https://askubuntu.com/questions/259744/to-move-all-file-with-certain-common-string-except-the-extension-of-the-file-to
+
+    cy.visit('/')
+    cy.get('p').contains('This Space Available').should('be.visible')
+    cy.get('a').contains('Become a Sponsor').click()
+    cy.location('pathname', { timeout: 10000 }).should('include', '/sponsor')
+    cy.wait(2000)
+    cy.get('h2').contains('Sponsor').should('be.visible')
+    cy.get('h3').contains('CHOOSE A WEEK').should('be.visible') 
   })
 
   it('ad shows when active', function() {
-    // cy.exec move md file into content
+    // cy.writeFile md file into content
+    // cy.writeFile('path/to/message.txt', 'Hello World')
+    // what happens when there is already a file there
   })
 
   it('can create ad', function() {
     // see Admin tests for how to stub out api response
   })
-
-  // Add to API test: can generate sponsor markdown from valid sponsor data from
 
 })

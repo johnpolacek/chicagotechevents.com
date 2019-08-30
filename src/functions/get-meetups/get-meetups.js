90c3cff361f052eb9123ae4033ef8eac27ff85e0
johnpolacek <johnpolacek@gmail.com>
2019-08-30 14:44:14 -0500

    test fetch meetups new api

--- src/functions/get-meetups/get-meetups.js
+++ src/functions/get-meetups/get-meetups.js
@@ -23,23 +23,41 @@ exports.handler = (event, context, callback) => {
     })
   }
 
-  meetup.getUpcomingEvents({
-    lat: 41.8781,
-    lon: -87.6298,
-    text: body.search,
-    offset: body.page || 0,
-    radius: 5
-  }, function(err, res) {
-      if (err) {
-        return callback(null, {
-          statusCode: 422,
-          body: JSON.stringify({ message: `Could not load events`, error: err, response: res })
-        })
-      } else {
-        return callback(null, {
-          statusCode: 200,
-          body: JSON.stringify({ message: `success`, response: res })
-        })
-      }
-  });  
+  const fetchUrl = 'https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public&lon=-87.6298&page=20&text=tech&radius=5&lat=41.8781'
+
+  return fetch(fetchUrl).then(res => {
+    return res.json()
+  }).then(data => {
+    console.log('data', data);
+    return  {
+      statusCode: 200,
+      body: JSON.stringify({ message: `success`, response: data, url: fetchUrl })
+    }
+  }).catch((error) => {
+    console.log('error', error)
+    return callback(null, {
+      statusCode: 400,
+      body: JSON.stringify(error)
+    })
+  })
+
+  // meetup.getUpcomingEvents({
+  //   lat: 41.8781,
+  //   lon: -87.6298,
+  //   text: body.search,
+  //   offset: body.page || 0,
+  //   radius: 5
+  // }, function(err, res) {
+  //     if (err) {
+  //       return callback(null, {
+  //         statusCode: 422,
+  //         body: JSON.stringify({ message: `Could not load events`, error: err, response: res })
+  //       })
+  //     } else {
+  //       return callback(null, {
+  //         statusCode: 200,
+  //         body: JSON.stringify({ message: `success`, response: res })
+  //       })
+  //     }
+  // });  
 }

015c207ece79c4533853d072ae4fae767441da6d
johnpolacek <johnpolacek@gmail.com>
2019-09-26 05:29:32 -0500

    update events query

--- gatsby-node.js
+++ gatsby-node.js
@@ -37,7 +37,7 @@ exports.createPages = ({ graphql, actions }) => {
       const previous = index === posts.length - 1 ? null : posts[index + 1].node
       const next = index === 0 ? null : posts[index - 1].node
 
-      if (post.node.fields && post.fields.slug) {
+      if (typeof(post.node.fields) !== 'undefined' && typeof(post.fields.slug) !== 'undefined') {
         createPage({
           path: post.node.fields.slug,
           component: eventListing,

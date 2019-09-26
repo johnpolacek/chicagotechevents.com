2104fb972d3160c7d50884212966a32dd848aaef
johnpolacek <johnpolacek@gmail.com>
2019-09-25 20:13:24 -0500

    add event page test

---
+++ cypress/integration/EventPage.js
@@ -0,0 +1,17 @@
+describe('Event Page', function() {
+  before(function() {
+    cy.unregisterServiceWorkers()
+  })
+
+  it('can be accessed from homepage', function() {
+    cy.visit('/')
+    // click first link
+    cy.get('h3 a').eq(0).click()
+    cy.location('pathname', { timeout: 10000 }).should('include', '/20') // starts with year. this will break in the year 2100
+    cy.wait(2000)
+    cy.get('h2').should('be.visible')
+    cy.get('a').contains('VIEW ALL EVENTS').click()
+    cy.location('pathname', { timeout: 10000 }).should('not.include', '/20')
+  })
+
+})
